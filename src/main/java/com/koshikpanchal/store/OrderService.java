fca811db8f93bc388814a2414529f288dae596c8
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-07-29 11:46:02 +0530

    Adding dependency injection using setter

--- src/main/java/com/koshikpanchal/store/OrderService.java
+++ src/main/java/com/koshikpanchal/store/OrderService.java
@@ -4,11 +4,16 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService (PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService (PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
     }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
 }
