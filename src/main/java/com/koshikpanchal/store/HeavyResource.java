44b2a24858f0804444d8387784c3e3855a54e895
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-07-29 15:12:05 +0530

    lazy initialization of classes using @Lazy

---
+++ src/main/java/com/koshikpanchal/store/HeavyResource.java
@@ -0,0 +1,13 @@
+package com.koshikpanchal.store;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class HeavyResource {
+    public HeavyResource() {
+        System.out.println("heavy resource created");
+        System.out.println("heavy resource created");
+    }
+}
