f1b52f859c174faa33addd8604ff75e6683a3925
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-07-31 12:47:45 +0530

    Added product controller with fetching via id and all product

---
+++ src/main/java/com/koshikpanchal/store/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.koshikpanchal.store.mapper;
+
+import com.koshikpanchal.store.dtos.ProductDto;
+import com.koshikpanchal.store.entity.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(source = "category.id", target = "categoryId")
+    ProductDto toDto(Product product);
+}
--- src/main/java/com/koshikpanchal/store/repositories/ProductRepository.java
+++ src/main/java/com/koshikpanchal/store/repositories/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.koshikpanchal.store.repositories;
 
 import com.koshikpanchal.store.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategory_Id(Byte categoryId);
+
+   @EntityGraph(attributePaths = "category")
+   @Query("SELECT p FROM Product p")
+   List<Product> findAllWithCategory();
 }
