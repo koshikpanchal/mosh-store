c2b8618ca7e4fdfb0fa746c087270b1505f811ee
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-04 16:49:33 +0530

    added endpoint for clearing the cart item

--- src/main/java/com/koshikpanchal/store/controllers/CartController.java
+++ src/main/java/com/koshikpanchal/store/controllers/CartController.java
@@ -112,4 +112,21 @@ public class CartController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @DeleteMapping("/{cartId}/items")
+    public ResponseEntity<?> clearCart(
+            @PathVariable("cartId") UUID cartId
+    ) {
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if(cart == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "cart was not found")
+            );
+        }
+        cart.clear();
+
+        cartRepository.save(cart);
+
+        return ResponseEntity.noContent().build();
+    }
 }
