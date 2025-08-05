6d03677cd6d1e57e8aaeb6577d67f3239d18e6e0
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-05 17:28:08 +0530

    added jwt token validation

--- src/main/java/com/koshikpanchal/store/controllers/AuthController.java
+++ src/main/java/com/koshikpanchal/store/controllers/AuthController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -35,6 +32,15 @@ public class AuthController {
         var token = jwtService.generateToken(request.getEmail());
 
         return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @PostMapping("/validate")
+    public boolean validate(
+            @RequestHeader("Authorization") String authHeader
+    ) {
+        var token = authHeader.replace("Bearer ", "");
+
+        return jwtService.validateToken(token);
     }
 
     public ResponseEntity<Void> handleBadCredentialsException() {
