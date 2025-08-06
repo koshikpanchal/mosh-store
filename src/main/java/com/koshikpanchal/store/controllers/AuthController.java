ea87480f0951eb85cfc49fb5390d75551dd0a6df
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-06 12:09:05 +0530

    added refresh end point to create new access token

--- src/main/java/com/koshikpanchal/store/controllers/AuthController.java
+++ src/main/java/com/koshikpanchal/store/controllers/AuthController.java
@@ -55,12 +55,19 @@ public class AuthController {
         return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
-    @PostMapping("/validate")
-    public boolean validate(
-            @RequestHeader("Authorization") String authHeader
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtResponse> refresh(
+            @CookieValue(value = "refreshToken") String refreshToken
     ) {
-        var token = authHeader.replace("Bearer ", "");
-        return jwtService.validateToken(token);
+        if (!jwtService.validateToken(refreshToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        var userId = jwtService.getUserIdFromToken(refreshToken);
+        var user = userRepository.findById(userId).orElseThrow();
+        var accessToken = jwtService.generateAccessToken(user);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
     @GetMapping("/me")
