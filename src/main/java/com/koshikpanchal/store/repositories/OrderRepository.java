58c5390aeea60e52eb6e70689c8906daf0be8e22
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-07 17:31:16 +0530

    added getAllOrder to controller

--- src/main/java/com/koshikpanchal/store/repositories/OrderRepository.java
+++ src/main/java/com/koshikpanchal/store/repositories/OrderRepository.java
@@ -8,10 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @EntityGraph(attributePaths = "items.product")
     @Query("select o from Order o where o.customer = :customer")
-    List<Order> getAllByCustomer(@Param("customer") User user);
+    List<Order> getOrdersByCustomer(@Param("customer") User user);
+
+    @EntityGraph(attributePaths = "items.product")
+    @Query("select o from Order o where o.id = :orderId")
+    Optional<Order> getOrderWithItems(@Param("orderId") Long orderId);
 }
--- src/main/java/com/koshikpanchal/store/services/OrderService.java
+++ src/main/java/com/koshikpanchal/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.koshikpanchal.store.services;
 
 import com.koshikpanchal.store.dtos.OrderDto;
+import com.koshikpanchal.store.exceptions.OrderNotFoundException;
 import com.koshikpanchal.store.mapper.OrderMapper;
 import com.koshikpanchal.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +19,16 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders() {
         var user = authService.getCurrentUser();
-        var order = orderRepository.getAllByCustomer(user);
+        var order = orderRepository.getOrdersByCustomer(user);
         return order.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository.getOrderWithItems(orderId).orElseThrow(OrderNotFoundException::new);
+        var user = authService.getCurrentUser();
+        if (!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You don't have access to this order");
+        }
+        return orderMapper.toDto(order);
     }
 }
