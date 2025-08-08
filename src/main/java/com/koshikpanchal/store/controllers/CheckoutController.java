b5c2932fcd93977bf4653162cdd06ee7223228e2
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-08-08 11:55:51 +0530

    exceptional handling for Stripe payment

--- src/main/java/com/koshikpanchal/store/controllers/CheckoutController.java
+++ src/main/java/com/koshikpanchal/store/controllers/CheckoutController.java
@@ -1,13 +1,14 @@
 package com.koshikpanchal.store.controllers;
 
 import com.koshikpanchal.store.dtos.CheckoutRequest;
-import com.koshikpanchal.store.dtos.CheckoutResponse;
 import com.koshikpanchal.store.dtos.ErrorDto;
 import com.koshikpanchal.store.exceptions.CartIsEmptyException;
 import com.koshikpanchal.store.exceptions.CartNotFoundException;
 import com.koshikpanchal.store.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,8 +20,15 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+
+        } catch (StripeException ex) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorDto("error creating a checkout session"));
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartIsEmptyException.class})
